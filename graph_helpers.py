822a2ce1815f67b0afd00674fc85cc258a030858
Alex Mandelias <alexmandelias@gmail.com>
2022-02-13 15:40:59 +0200

    add Config class to simplify configuration

--- graph_helpers.py
+++ graph_helpers.py
@@ -1,6 +1,8 @@
-import os
+from __future__ import annotations
+
 import sys
 
+from config import Config
 from file_util import writer
 from main import get_results_from_all_classifications
 from timed import Timer
@@ -9,12 +11,14 @@ Priority = Timer.Priority
 
 
 @Timer(priority=0, prompt="Get Plotting Data")
-def get_plotting_data(data_dir: str) -> tuple[list[int], list[float], list[float], list[float]]:
+def get_plotting_data(config: Config) -> dict[str, list | dict[str, list]]:
     """
+    TODO
+
     Get the accuracy of repeated executions of the ID3 and Random Forest classifiers with
     variable parameters.
 
-    :param data_dir: the directory containing the training examples
+    :param config: TODO
     :return: A tuple containing 4 lists of floats. More specifically:
 
     0: A list with the accuracy of the ID3 tree using the training data
@@ -23,28 +27,26 @@ def get_plotting_data(data_dir: str) -> tuple[list[int], list[float], list[float
     3: A list with the accuracy of Random Forest using the testing data
     """
 
-    train_data_dir = os.path.join(data_dir, "train")
-    test_data_dir = os.path.join(data_dir, "test")
-    vocab_file_name = os.path.join(data_dir, "imdb.vocab")
+    results: dict[str, list | dict[str, list]] = dict()
+    results['counts']: list = []
+    results['id3']: dict[str, list] = {'tr': [], 'te': []}
+    results['rf']: dict[str, list] = {'tr': [], 'te': []}
 
-    results = dict()
-    results['counts'] = []
-    results['id3'] = {'tr': [], 'te': []}
-    results['rf'] = {'tr': [], 'te': []}
+    config['ignored_attributes'] = 12
+    config['attribute_count'] = 200
 
-    ignored_attrs = 12
-    count_attrs = 200
-    
-    print("ID3 - R/F - TRA - TES - Count - {:^10s} - {:^10s} - {:^10s} - {:^10s} ".format("Accuracy", "Precision", "Recall", "F1"))
+    fmt = "ID3 - R/F - TRA - TES - Count" + 4 * " - {:^10s}"
+    print(fmt.format("Accuracy", "Precision", "Recall", "F1"))
 
     for example_count in [250, 500] + list(range(1000, 10001, 1000)):
-        result = get_results_from_all_classifications(train_data_dir, test_data_dir, vocab_file_name, example_count, count_attrs,
-                                                      ignored_attrs)
+        config['example_count'] = example_count
+        result = get_results_from_all_classifications(config)
+
         id3tr = result.id3_tr
         id3te = result.id3_te
         frtr = result.rf
         frte = result.rf
-        
+
         r = id3tr
         print(" X  -     -  X  -     - ", end="")
         print("%5d - %.8f - %.8f - %.8f - %.8f" % (example_count, r.accuracy(), r.precision(), r.recall(), r.f_measure()))
@@ -57,7 +59,7 @@ def get_plotting_data(data_dir: str) -> tuple[list[int], list[float], list[float
         r = frte
         print("    -  X  -     -  X  - ", end="")
         print("%5d - %.8f - %.8f - %.8f - %.8f" % (example_count, r.accuracy(), r.precision(), r.recall(), r.f_measure()))
-        
+
         results['counts'].append(example_count)
         results['id3']['tr'].append(id3tr)
         results['id3']['te'].append(id3te)
@@ -68,7 +70,7 @@ def get_plotting_data(data_dir: str) -> tuple[list[int], list[float], list[float
 
 
 @Timer(priority=Priority.PRODUCTION, prompt="Write data to csv")
-def write_data_to_files(data, output_file_name: str, methods: list[str], args: dict[str, list]) -> None:
+def write_data_to_files(data, output_file_name: str, methods: list[str], args: dict[str, tuple]) -> None:
     """
     TODO
 
@@ -97,12 +99,20 @@ def write_data_to_files(data, output_file_name: str, methods: list[str], args: d
 
 @Timer(priority=Priority.PRODUCTION, prompt="Get Data for Graphs")
 def main():
-    try:
-        data = get_plotting_data(sys.argv[1])
-        write_data_to_files(data, sys.argv[2])
-    except IOError as ioe:
-        print("Error: ", ioe)
+    """ TODO """
+
+    if len(sys.argv) != 3:
+        print("Usage:\npython graph_helpers.py <config file> <output file>")
+        return
+
+    config = Config(sys.argv[1])
+
+    data = get_plotting_data(config)
+    write_data_to_files(data, sys.argv[2])
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as exception:
+        print(f"An Exception occurred:\n\n{exception}", file=sys.stderr)
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import os
 import sys
 
 from classifier_evaluation import ClassifierEvaluation
-from load_imdb import load_all_examples, load_all_attributes, filter_attributes_by_examples
+from config import Config
+from load_imdb import load_all_examples, load_all_attributes,\
+    filter_attributes_by_examples
 from classifier import Classifier, Example
 from id3 import ID3
 from random_forest import RandomForest
@@ -13,22 +14,26 @@ CE = ClassifierEvaluation
 
 
 @Timer(priority=Priority.RUN)
-def load_data(dir_tr: str, dir_te: str, vocab_file: str, example_count: int, attr_count: int, ignored_attr_count: int):
+def load_data(config: Config):
     """ TODO """
 
-    examples_tr = load_all_examples(dir_tr, example_count)
-    examples_te = load_all_examples(dir_te, example_count)
-    attributes = load_all_attributes(vocab_file, attr_count, ignored_attr_count)
+    examples_tr = load_all_examples(config["train"], config["example_count"])
+    examples_te = load_all_examples(config["test"], config["example_count"])
+
+    attributes = load_all_attributes(config["vocab"],
+                                     config["attribute count"],
+                                     config["ignored_attributes"])
     attributes = filter_attributes_by_examples(attributes, examples_tr)
 
     return examples_tr, examples_te, attributes
 
 
 @Timer(priority=Priority.RUN)
-def get_trained_classifiers(examples, attributes) -> tuple[ID3, RandomForest]:
+def get_trained_classifiers(examples: set[Example], attributes: set[str]) ->\
+        tuple[ID3, RandomForest]:
     """ TODO """
 
-    # no need to copy the examples since random forest makes a copy for each tree
+    # no need to copy the Examples, random forest makes a copy for each tree
     id3 = ID3(examples, attributes)
     random_forest = RandomForest(examples, attributes)
 
@@ -36,7 +41,7 @@ def get_trained_classifiers(examples, attributes) -> tuple[ID3, RandomForest]:
 
 
 @Timer(priority=Priority.RUN)
-def get_stats_from_classifier(classifier: Classifier, examples: set[Example]) -> CE:
+def classify_and_stats(classifier: Classifier, examples: set[Example]) -> CE:
     """
     TODO
 
@@ -45,7 +50,7 @@ def get_stats_from_classifier(classifier: Classifier, examples: set[Example]) ->
     :return: a ClassifierEvaluation object with the results of the classification
     """
 
-    classifier.classify_bulk(examples)
+    classifier.classify_bulk([Example.copy_of(e) for e in examples])
 
     return CE(examples)
 
@@ -61,62 +66,38 @@ class Results:
 
 
 @Timer(priority=Priority.RUN)
-def get_results_from_all_classifications(dir_tr: str, dir_te: str, vocab_file: str, example_count: int, attr_count: int, ignored_attr_count: int) -> Results:
+def get_results_from_all_classifications(config: Config) -> Results:
+    """ TODO """
 
-    data = load_data(dir_tr, dir_te, vocab_file, example_count, attr_count, ignored_attr_count)
-    examples_tr, examples_te, attributes = data
-
+    examples_tr, examples_te, attributes = load_data(config)
     id3, rand_forest = get_trained_classifiers(examples_tr, attributes)
 
-    return Results(get_stats_from_classifier(id3, examples_tr), get_stats_from_classifier(id3, examples_te),
-                   get_stats_from_classifier(rand_forest, examples_tr), get_stats_from_classifier(rand_forest, examples_te))
+    return Results(classify_and_stats(id3, examples_tr),
+                   classify_and_stats(id3, examples_te),
+                   classify_and_stats(rand_forest, examples_tr),
+                   classify_and_stats(rand_forest, examples_te))
 
 
 @Timer(priority=Priority.PRODUCTION, prompt="Main program")
 def main() -> None:
-    def check_int_arg(arg: str, param_name: str, bottom_limit: int, upper_limit: int) -> int:
-        try:
-            number = int(arg)
-        except ValueError:
-            number = -1
+    """ TODO """
 
-        if number < bottom_limit or number > upper_limit:
-            print(f"Error: parameter `{param_name}` must be a valid integer within the "
-                  f"[{bottom_limit}, {upper_limit}] range")
-            sys.exit(1)
-        else:
-            return number
+    if len(sys.argv) != 2:
+        print("Usage:\npython main.py <config file>")
+        return
 
-    if len(sys.argv) < 5:
-        print("Insufficient parameters:")
-        print("Use main.py <imdb directory> <number of examples to be loaded>"
-              " <number of ignored words> <number of words to be considered>")
-    else:
-        data_dir = sys.argv[1]
-        train_data_dir = os.path.join(data_dir, "train")
-        test_data_dir = os.path.join(data_dir, "test")
-        vocab_file_name = os.path.join(data_dir, "imdb.vocab")
+    config = Config(sys.argv[1])
 
-        example_size = check_int_arg(sys.argv[2], "example size", 100, 250000)
-        ignore_attr_count = check_int_arg(sys.argv[3], "ignored words count", 0, 90000)
-        attr_count = check_int_arg(sys.argv[4], "total word count", 5, 90000)
+    results = get_results_from_all_classifications(config)
 
-        if attr_count > 200:
-            answer = input("Warning: Giving more than 200 words as attributes is likely to make the algorithm "
-                           "incredibly inconsistent. Are you sure you want to proceed? (y/n)")
-            if answer.lower() != "y":
-                sys.exit(0)
-
-        results = get_results_from_all_classifications(train_data_dir, test_data_dir, vocab_file_name, example_size, attr_count, ignore_attr_count)
-        print("ID3 training results: ", results.id3_tr.basic_stats())
-        print("ID3 testing results: ", results.id3_te.basic_stats())
-        print("Random Forest training results: ", results.rf.basic_stats())
-        print("Random Forest testing results: ", results.rf.basic_stats())
+    print("ID3 training results: ", results.id3_tr.basic_stats())
+    print("ID3 testing results: ", results.id3_te.basic_stats())
+    print("Random Forest training results: ", results.rf.basic_stats())
+    print("Random Forest testing results: ", results.rf.basic_stats())
 
 
 if __name__ == "__main__":
     try:
         main()
-    except IOError as ioe:
-        print("Error: ", ioe)
-        print("Please make sure the first argument is the upper-most directory ('aclImdb') containing the imdb dataset")
+    except Exception as exception:
+        print(f"An Exception occurred:\n\n{exception}", file=sys.stderr)
